04fcf4378a8f4cc66e07f384e03e4fb9f06959f6
Shelly21293 <97115805+Shelly21293@users.noreply.github.com>
2023-01-10 21:43:26 +0200

    Update Home.js

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <img style={{ height: '100%', width: '100%', backgroundSize: "cover" }} src="http://127.0.0.1:8000/media/homeBG/homebg2.jpeg"></img>
+            <img style={{ height: '100%', width: '100%', backgroundSize: "cover" }} src="https://project-patisserie.onrender.com/media/homeBG/homebg2.jpeg"></img>
         </div>
     )
 }
